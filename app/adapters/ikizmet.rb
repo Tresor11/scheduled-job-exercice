f20086d71eb93b1b8e65606b610b50fe0d37deef
Tresor <tresor.moise2001@gmail.com>
2021-10-17 13:17:55 +0200

    create api adapter

---
+++ app/adapters/ikizmet.rb
@@ -0,0 +1,40 @@
+ class Ikizmet
+
+  API_URL = 'https://dashboard-qa.ikizmet.com/api/v2/tests'
+
+  def initialize
+  end
+
+  def client_info
+    request = get("#{API_URL}/client_info.json")
+    request.run
+    request.reponse
+  end
+
+  def client_sales
+    request = get("#{API_URL}/client_sales.json")
+    request.run
+    request.reponse
+  end
+
+  private
+
+  def get(url)
+    request(url)
+  end
+
+  def request(*options)
+    Typhoeus.before do |request|
+      request.on_complete do |response|
+        if response.success? && response.body.present?
+          JSON.parse(response.body)
+        else
+          {}
+        end
+      end
+    end
+
+    Typhoeus::Request.new(*options)
+  end
+
+ end
